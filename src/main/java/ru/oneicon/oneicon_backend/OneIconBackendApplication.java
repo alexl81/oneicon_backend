c49bbfad17a6316722e134def8e3253d8b77149c
Alex Lapin <lapinalexandr@yandex.ru>
2021-05-31 19:56:34 +0300

    Start development 1. Create DataBase oneIcon 2. Creating DataSource 3. Adding spring-boot dependencies: spring-web, spring data JPA, lombok, postgres driver 4. configure application. properties

---
+++ src/main/java/ru/oneicon/oneicon_backend/OneIconBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.oneicon.oneicon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneIconBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneIconBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/oneicon/oneicon_backend/OneIconBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oneicon.oneicon_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneIconBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
